727059bd4e3c4cf6084912d6813d428cdfb2485c
steptoreal <jcob0920@gmail.com>
2023-01-17 23:42:25 +0900

    $5 + 10CHF = $10 (환율이 2:1 인 경우) 에 대한 기능 구현

--- DollarProject/src/main/java/tdd/exam/Sum.java
+++ DollarProject/src/main/java/tdd/exam/Sum.java
@@ -1,21 +1,26 @@
 package tdd.exam;
 
 public class Sum implements Expression{
-    Money augend;
-    Money addend;
+    Expression augend;
+    Expression addend;
 
-    Sum(Money augend, Money addend) {
+    Sum(Expression augend, Expression addend) {
         this.augend = augend;
         this.addend = addend;
     }
 
     public Money reduce(String to) {
-        int amount = augend.amount + addend.amount;
-        return new Money(amount, to);
+//        int amount = augend.amount + addend.amount;
+//        return new Money(amount, to);
+        return null;
     }
 
     public Money reduce(Bank bank, String to) {
-        int amount = augend.amount + addend.amount;
+        int amount = augend.reduce(bank, to).amount + addend.reduce(bank, to).amount;
         return new Money(amount, to);
+    }
+
+    public Expression plus(Expression addend) {
+        return null;
     }
 }
