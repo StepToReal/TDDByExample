521689552529814ba0102dd461e0a4e21a7871be
steptoreal <jcob0920@gmail.com>
2023-02-25 15:22:11 +0900

    DollarProject 불필요한 코드 정리

--- DollarProject/src/main/java/tdd/exam/Sum.java
+++ DollarProject/src/main/java/tdd/exam/Sum.java
@@ -9,12 +9,6 @@ public class Sum implements Expression{
         this.addend = addend;
     }
 
-    public Money reduce(String to) {
-//        int amount = augend.amount + addend.amount;
-//        return new Money(amount, to);
-        return null;
-    }
-
     public Money reduce(Bank bank, String to) {
         int amount = augend.reduce(bank, to).amount + addend.reduce(bank, to).amount;
         return new Money(amount, to);
